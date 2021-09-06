455b980afe17e861876c295eb2bda7497e873299
Danny Woodford <dwoodford@brandgage.com>
2021-09-06 00:43:34 -0400

    update package.json

--- src/js/CANVAS/containers/Canvas.js
+++ src/js/CANVAS/containers/Canvas.js
@@ -1,8 +1,7 @@
 import React, { Suspense } from 'react'
-import { Stats } from 'drei'
-import { Canvas } from 'react-three-fiber'
+import { Stats } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 
-import Projects from './Projects.js'
 
 import Loading from '../components/setup/Loading.js'
 import FireworkSolidColored from '../components/models/FireworkSolidColored.js'
@@ -10,7 +9,12 @@ import FireworkSolidColored from '../components/models/FireworkSolidColored.js'
 export default function Scene({ ...props }) {
 
 	return (
-		<Canvas style={{height: '100%', width:'100%'}} camera={{ fov: 80 }}>
+		<Canvas 
+			AdaptivePixelRatio
+			concurrent
+			style={{height: '100%', width:'100%'}} 
+			camera={{ fov: 80 }}
+		>
 			<Suspense fallback={<Loading />}>
 				<FireworkSolidColored />
 				{/* <Projects /> */}
