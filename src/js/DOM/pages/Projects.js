6e6a71b9621a984eaa6d132d44c09cd6178a0e9b
Danny Woodford <dwoodford@brandgage.com>
2021-10-16 19:16:12 -0400

    CHANGED font changed container to scroll properly

--- src/js/DOM/pages/Projects.js
+++ src/js/DOM/pages/Projects.js
@@ -28,130 +28,136 @@ export default function Projects({...props}) {
                 transition: 'opacity 2s ease-in-out .6s',
                 opacity: menuStateCurrentName === 'projects' ? 1 : 0,
             }}
-        >
-            <div className='container'>
-				<div className="item medium">
-					<div className="item__border">
-						<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/flatiron-logo.png'}/>
-						<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/flatiron-gif.gif'} />
-					</div>
-				</div>
-				<div className="item medium">
-					<div className="item__border">
-						<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/brandgage-logo.png'}/>
-						<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/brandgage-gif.gif'} />
-					</div>
-				</div>  
-				<div className="item medium">
-					<div className="item__border">
-						<img className="item__image inver-on-hover" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/projects-logo.png'}/>
-						<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/projects-gif.gif'} />
-					</div>
-				</div>  
-				<div className="double-item">
-					<div className="item">
+        >	
+            <div className='max-width-container'>
+				<div className='container'>
+					<div className="item medium">
 						<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/blender-logo.png'}/>
-							<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/blender-gif.gif'} />
+							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/flatiron-logo.png'}/>
+							<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/flatiron-gif.gif'} />
 						</div>
 					</div>
-					<div className="item" onClick={() => props.toggleMenuHandler()}>
-						<NavLink to='/home' className="nav-link">
+					<div className="item medium">
+						<div className="item__border">
+							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/brandgage-logo.png'}/>
+							<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/brandgage-gif.gif'} />
+						</div>
+					</div>  
+					<div className="item medium">
+						<div className="item__border">
+							<img className="item__image inver-on-hover" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/projects-logo.png'}/>
+							<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/projects-gif.gif'} />
+						</div>
+					</div>  
+					<div className="double-item">
+						<div className="item">
+							<div className="item__border">
+								<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/blender-logo.png'}/>
+								<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/blender-gif.gif'} />
+							</div>
+						</div>
+						<div className="item" onClick={() => props.toggleMenuHandler()}>
+							<NavLink to='/home' className="nav-link">
+								<div className="item__border">
+									<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/art-logo.png'}/>
+									<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/art-gif.gif'} />
+								</div>
+							</NavLink>
+						</div>
+					</div>
+					<div className="item large is-welcome-item">
+							<div className="item__border">
+								<div className="text">
+									<div id='name'><p>DANNY WOODFORD</p></div>
+									<div id='occupation'><p>3D Web Developer</p></div>
+								</div>
+								{/* <img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/welcome-logo.png'}/> */}
+								{/* <img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/projects-gif.gif'} /> */}
+							</div>
+					</div>
+					<div className="double-item">
+						<div className="item">
 							<div className="item__border">
 								<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/art-logo.png'}/>
 								<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/art-gif.gif'} />
 							</div>
-						</NavLink>
-					</div>
-				</div>
-				<div className="item large is-welcome-item">
-						<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/welcome-logo.png'}/>
-							{/* <img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/projects-gif.gif'} /> */}
 						</div>
-				</div>
-				<div className="double-item">
-					<div className="item">
-						<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/art-logo.png'}/>
-							<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/art-gif.gif'} />
+						<div className="item">
+							<div className="item__border">
+								<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/blender-logo.png'}/>
+								<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/blender-gif.gif'} />
+							</div>
 						</div>
 					</div>
 					<div className="item">
 						<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/blender-logo.png'}/>
-							<img alt="" className="item__image hover-image" src={process.env.PUBLIC_URL + '/gifs/projects/blender-gif.gif'} />
-						</div>
-					</div>
-				</div>
-				<div className="item">
-					<div className="item__border">
-						<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/about-me-logo.png'}/>
-						<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
-					</div>
-				</div>
-				<a href={process.env.PUBLIC_URL + '/Danny_Woodford_Resume.pdf'} target='_blank' rel='noopener noreferrer' className='display-contents'>
-					<div className="item small">
-						<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/resume-logo.png'}/>
+							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/about-me-logo.png'}/>
 							<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
 						</div>
 					</div>
-				</a>
-				<span 
-					onPointerOut={() => resetTooltip()}
-					className='display-contents'
-				>  
-					<CopyToClipboard 
-						onCopy={() => setIconHasBeenCopied({copyPhoneNumber: true})} 
-						className='item small' 
-						text={'9089078680'} 
-						data-for='copyPhoneNumber' 
-						data-tip=''
-						data-border={true} 
-						data-border-color='white' 
-						data-text-color='white' 
-					>
-						<div className="item small" onPointerOut={() => resetTooltip()}>
+					<a href={process.env.PUBLIC_URL + '/Danny_Woodford_Resume.pdf'} target='_blank' rel='noopener noreferrer' className='display-contents'>
+						<div className="item small">
 							<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/phone-logo.png'}/>
-						<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
+								<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/resume-logo.png'}/>
+								<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
 							</div>
-						</div>  
-					</CopyToClipboard>
-					<ReactTooltip id='copyPhoneNumber' getContent={() => iconHasBeenCopied.copyPhoneNumber ? 'Phone number has been copied to clipboard!' : 'Copy phone number to clipboard'}/>
-				</span>
-				<span 
-					onPointerOut={() => resetTooltip()}
-					className='display-contents'
-				>  
-					<CopyToClipboard 
-						onCopy={() => setIconHasBeenCopied({copyEMail: true})} 
-						className='item small' 
-						text={'daniel.woodford.g@gmail.com'} 
-						data-for='copyEMail' 
-						data-tip=''
-						data-border={true} 
-						data-border-color='white' 
-						data-text-color='white' 
-					>
-						<div className="item small" onPointerOut={() => resetTooltip()}>
-							<div className="item__border">
-									<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/email-logo.png'}/>
-									<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
-							</div>
-						</div>  
-					</CopyToClipboard>
-					<ReactTooltip id='copyEMail' getContent={() => iconHasBeenCopied.copyEMail ? 'E-mail has been copied to clipboard!' : 'Copy e-mail to clipboard'}/>
-				</span>
-				<a href='https://www.linkedin.com/in/danny-woodford-54b418126/' target='_blank' rel='noopener noreferrer' className='display-contents'>
-					<div className="item">
-						<div className="item__border">
-							<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/linkedIn-logo.png'}/>
-							<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
 						</div>
-					</div>
-				</a>
+					</a>
+					<span 
+						onPointerOut={() => resetTooltip()}
+						className='display-contents'
+					>  
+						<CopyToClipboard 
+							onCopy={() => setIconHasBeenCopied({copyPhoneNumber: true})} 
+							className='item small' 
+							text={'9089078680'} 
+							data-for='copyPhoneNumber' 
+							data-tip=''
+							data-border={true} 
+							data-border-color='white' 
+							data-text-color='white' 
+						>
+							<div className="item small" onPointerOut={() => resetTooltip()}>
+								<div className="item__border">
+								<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/phone-logo.png'}/>
+							<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
+								</div>
+							</div>  
+						</CopyToClipboard>
+						<ReactTooltip id='copyPhoneNumber' getContent={() => iconHasBeenCopied.copyPhoneNumber ? 'Phone number has been copied to clipboard!' : 'Copy phone number to clipboard'}/>
+					</span>
+					<span 
+						onPointerOut={() => resetTooltip()}
+						className='display-contents'
+					>  
+						<CopyToClipboard 
+							onCopy={() => setIconHasBeenCopied({copyEMail: true})} 
+							className='item small' 
+							text={'daniel.woodford.g@gmail.com'} 
+							data-for='copyEMail' 
+							data-tip=''
+							data-border={true} 
+							data-border-color='white' 
+							data-text-color='white' 
+						>
+							<div className="item small" onPointerOut={() => resetTooltip()}>
+								<div className="item__border">
+										<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/email-logo.png'}/>
+										<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
+								</div>
+							</div>  
+						</CopyToClipboard>
+						<ReactTooltip id='copyEMail' getContent={() => iconHasBeenCopied.copyEMail ? 'E-mail has been copied to clipboard!' : 'Copy e-mail to clipboard'}/>
+					</span>
+					<a href='https://www.linkedin.com/in/danny-woodford-54b418126/' target='_blank' rel='noopener noreferrer' className='display-contents'>
+						<div className="item">
+							<div className="item__border">
+								<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + '/images/projects/thumbnails/linkedIn-logo.png'}/>
+								<img alt="" className="item__image hover-image" src="https://prod-ripcut-delivery.disney-plus.net/v1/rawFiles/disney/RAW_59F432D7A600A8F1CE36AE4D0B96570AC7C8277A0E8855BC06FABBAE090E6577" />
+							</div>
+						</div>
+					</a>
+				</div>
             </div>
         </div>
     )
