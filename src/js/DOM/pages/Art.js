67a97b52dcf814ee48603497080041b8406ea32e
Danny Woodford <dwoodford@brandgage.com>
2021-10-19 16:45:55 -0400

    Simplify pages and add react-responsive-carousel

--- src/js/DOM/pages/Art.js
+++ src/js/DOM/pages/Art.js
@@ -1,62 +1,63 @@
-import React, { useState} from 'react'
+import React from 'react'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 export default function Art({...props}) {
 
     const menuStateCurrentName = props.menuState.currentName
 
-	const artArray = [
+	const itemArray = [
 		{
 			name: 'art-1',
 			type: 'image',
-			png: '/images/art/art-1.jpeg'
+			url: '/images/art/art-1.jpeg'
 		},
 		{
 			name: 'art-2',
 			type: 'image',
-			png: '/images/art/art-2.jpeg'
+			url: '/images/art/art-2.jpeg'
+		},
+		{
+			name: 'art-video',
+			type: 'video',
+			url: '/videos/Chess-demo.mp4'
 		},
 		{
 			name: 'art-3',
 			type: 'image',
-			png: '/images/art/art-3.jpeg'
+			url: '/images/art/art-3.jpeg'
 		},
 		{
 			name: 'art-4',
 			type: 'image',
-			png: '/images/art/art-4.jpeg'
+			url: '/images/art/art-4.jpeg'
 		},
 		{
 			name: 'art-5',
 			type: 'image',
-			png: '/images/art/art-5.JPG'
+			url: '/images/art/art-5.JPG'
 		},
 		{
 			name: 'art-6',
 			type: 'image',
-			png: '/images/art/art-6.JPG'
+			url: '/images/art/art-6.JPG'
 		},
 		{
 			name: 'art-7',
 			type: 'image',
-			png: '/images/art/art-7.JPG'
+			url: '/images/art/art-7.JPG'
 		},
 	]
 
-	const displayArtItems = artArray.map(item => (
-		<div className="item" key={item.name} onClick={() => updateDisplayArt(item.png)}>
-			<div className="item__border">
-				<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + item.png }/>
-			</div>
+	const displayItems = itemArray.map(item => (
+		<div key={item.name}>
+			{item.type === 'image' ? 
+				<img src={process.env.PUBLIC_URL + item.url } alt={item.name} /> : 
+				<video src={process.env.PUBLIC_URL + item.url } className='videoItem' autoPlay loop muted ></video>
+			}
 		</div>
-	));
+	))
 
-	const [displayArtSrc, setDisplayArtSrc] = useState('/images/art/sunrise.png')
-	const [displayArtSelected, setDisplayArtSelected] = useState(false)
-
-	const updateDisplayArt = (itemSrc) => {
-		setDisplayArtSrc(itemSrc)
-		setDisplayArtSelected(true)
-	}
     return (
         <div className='page art-page'
 			style={{
@@ -66,12 +67,20 @@ export default function Art({...props}) {
 		>
 			<div className='max-width-container'>
 				<div className='container'>
-					<div className='art-container'>
-						<div className="display-art">
-							<img className="display-art-image" alt="" src={process.env.PUBLIC_URL + displayArtSrc} style={{opacity: !displayArtSelected ? 0 : 1}}/>
-						</div>
+					<div className='page-container'>
 						<h2>I've added a few paintings/drawings I've done which may not seem relevant but gives credence to my visual sense which I believe to be hugely important to animation and 3D in general because of their visual/spacial nature.</h2>
-						{displayArtItems}
+						<div className="display-item">
+							<Carousel 
+								emulateTouch={true} 
+								swipeable={true}
+								dynamicHeight={true} 
+								infiniteLoop={false} 
+								showThumbs={false}
+								autoPlay={false} 
+							>
+								{displayItems}
+							</Carousel>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/js/DOM/pages/Projects.js
+++ src/js/DOM/pages/Projects.js
@@ -1,8 +1,61 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 export default function Projects({...props}) {
-
+    
     const menuStateCurrentName = props.menuState.currentName
+	const [displayItemSrc, setDisplayItemSrc] = useState('/images/placeholder.jpg')
+	const [displayItemSelected, setDisplayItemSelected] = useState(false)
+
+	const itemArray = [
+		{
+			name: 'art-1',
+			type: 'image',
+			url: '/images/art/art-1.jpeg'
+		},
+		{
+			name: 'art-2',
+			type: 'image',
+			url: '/images/art/art-2.jpeg'
+		},
+		{
+			name: 'art-3',
+			type: 'image',
+			url: '/images/art/art-3.jpeg'
+		},
+		{
+			name: 'art-4',
+			type: 'image',
+			url: '/images/art/art-4.jpeg'
+		},
+		{
+			name: 'art-5',
+			type: 'image',
+			url: '/images/art/art-5.JPG'
+		},
+		{
+			name: 'art-6',
+			type: 'image',
+			url: '/images/art/art-6.JPG'
+		},
+		{
+			name: 'art-7',
+			type: 'image',
+			url: '/images/art/art-7.JPG'
+		},
+	]
+
+	const displayItems = itemArray.map(item => (
+		<div className="item" key={item.name} onClick={() => updateDisplayItem(item.url)}>
+			<div className="item__border">
+				<img className="item__image" draggable="false" alt="" src={process.env.PUBLIC_URL + item.url }/>
+			</div>
+		</div>
+	))
+
+	const updateDisplayItem = (itemSrc) => {
+		setDisplayItemSrc(itemSrc)
+		setDisplayItemSelected(true)
+	}
 
     return (
         <div className='page projects-page'
@@ -13,9 +66,12 @@ export default function Projects({...props}) {
 		>
 			<div className='max-width-container'>
 				<div className='container'>
-					<div className='projects-container' >
-						<h1>projects</h1>
-						<p>Former carpenter turned full-stack developer in the pursuit of more challenging and ornate work. Renovating and building houses exposed a passion for engineering and building where I learned to see big jobs through from stPersonalProjects to finish independently. I graduated from the Flatiron School where I learned JavaScript, React, Ruby, and Ruby on Rails mainly, and discovered canvas for the first time. While there I stPersonalProjectsed and lead the first cross-discipline collaboration event. In my personal life, I like to write jokes and make things like paintings, replica movie props, woodworking projects for fun. I am a semi-professional stand-up comedian and a pPersonalProjects from doing it for the love of it I believe to grow as a person you must chase the things that make you inherently anxious such as expressing ideas publicly or overcoming failure; along with creative problem solving, skills I now consider to be some of my greatest strengths.</p>
+					<div className='page-container'>
+						<div className="display-item">
+							<img className="display-item-image" alt="" src={process.env.PUBLIC_URL + displayItemSrc} style={{opacity: !displayItemSelected ? 0 : 1}}/>
+						</div>
+						<h2>I've added a few paintings/drawings I've done which may not seem relevant but gives credence to my visual sense which I believe to be hugely important to animation and 3D in general because of their visual/spacial nature.</h2>
+						{displayItems}
 					</div>
 				</div>
 			</div>
